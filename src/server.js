7e08095bb6dd05488bd0e20a5abae581376c1de7
Luiz <grexd@hotmail.com>
2022-05-10 01:27:56 -0300

    Add bar code error validation

--- src/server.js
+++ src/server.js
@@ -5,7 +5,28 @@ const app = express();
 app.use(express.json());
 
 app.get('/api/teste/:barCode', (request, response) => {
+
+	const barCode = request.params.barCode;
+	intCode = parseInt(barCode);
 	
+	if(isNaN(intCode)) {
+		return response
+			.status(400)
+			.json({ statusCode: 400, message: 'O codigo deve conter somente numero' });
+	}
+
+	if(barCode.length < 44) {
+		return response
+			.status(400)
+			.json({ statusCode: 400, message: 'Quantidade de digitos inferior a padrão' });
+	}
+	
+	if(barCode.length > 44) {
+		return response
+			.status(400)
+			.json({ statusCode: 400, message: 'Quantidade de digitos superior a padrão' });
+	}
+
 });
 
 app.listen(1234, () => console.log('Listening on 1234'));
